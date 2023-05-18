4217240b7d35b74ce89e20de851bd0a8528c2488
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-05-19 00:13:47 +0200

    i do think we have it now

--- app.py
+++ app.py
@@ -2,30 +2,65 @@ import streamlit as st
 from os import path
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain import PromptTemplate, LLMChain
+from langchain import OpenAI, LLMChain, PromptTemplate
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
-from langchain.schema import HumanMessage, SystemMessage, AIMessage
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.callbacks.base import BaseCallbackHandler
 
-load_dotenv()
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.chains import RetrievalQAWithSourcesChain, ConversationalRetrievalChain
 
-storage_dir = "./storage/nemisa"
+import chromadb
+
+# load environment variables
+load_dotenv()
 
 st.set_page_config(
     page_title="Gold Diggin'",
     layout="wide",
 )
 
-# Initialize session state
+storage_dir = "./storage/nemisa"
+documents_dir = "./data/nemisa"
+# read the files;
 
+embeddings = OpenAIEmbeddings()
 
-st.title("Golden, ⛏️")
+client_settings = chromadb.config.Settings(
+    chroma_db_impl="duckdb+parquet",
+    persist_directory=storage_dir,
+    anonymized_telemetry=False
+)
+
+if path.exists(storage_dir + '/index'):
+    print('Loading from storage')
+    vectordb = Chroma(
+        collection_name="nemisa_annaul_reports",
+        persist_directory=storage_dir,
+        client_settings=client_settings,
+        embedding_function=embeddings
+    )
+else:
+    print('Creating new index')
+    loader = PyPDFDirectoryLoader(documents_dir)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=20)
+    texts = text_splitter.split_documents(documents)
+    vectordb = Chroma(
+        collection_name="nemisa_annaul_reports",
+        persist_directory=storage_dir,
+        client_settings=client_settings,
+        embedding_function=embeddings
+    )
+    vectordb.add_documents(documents=texts, embedding=embeddings)
+    vectordb.persist()
 
 
 class SessionState:
@@ -56,6 +91,47 @@ class MyStreamingCallBackHandler(BaseCallbackHandler):
         # print(token, end="", flush=True)
 
 
+chat_model = ChatOpenAI(streaming=True, callbacks=[MyStreamingCallBackHandler()],
+                        temperature=0, model_name="gpt-3.5-turbo")
+
+
+questions = []
+chat_history = []
+
+qAndy = ConversationalRetrievalChain.from_llm(
+    llm=chat_model,
+    retriever=vectordb.as_retriever(),
+    verbose=True,
+    # qa_prompt=QA_PROMPT,
+    # condense_question_prompt=CONDENSE_QUESTION_PROMPT
+)
+
+st.title("Golden, ⛏️")
+question = st.text_input(
+    "Ask a question")
+
+output = st.empty()
+
+if question:
+    result = qAndy({"question": question, "chat_history": chat_history})
+    chat_history.append((question, result['answer']))
+
+exit()
+
+"""
+if question:
+    questions.append(question)
+    chat_history.append((question, result['answer']))
+    print(result)
+    # mm = chain.run(text=query)
+
+"""
+
+
+# Initialize session state
+st.title("Golden, ⛏️")
+
+
 chat = ChatOpenAI(streaming=True, callbacks=[MyStreamingCallBackHandler()],
                   temperature=0.4, model_name="gpt-3.5-turbo")
 
@@ -69,13 +145,17 @@ chat_prompt = ChatPromptTemplate.from_messages(
 
 chain = LLMChain(llm=chat, prompt=chat_prompt)
 
+# qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type='stuff', vectorstore=vectordb)
+qa = RetrievalQAWithSourcesChain.from_chain_type(
+    llm=OpenAI(), chain_type='stuff', retriever=vectordb.as_retriever())
+
 query = st.text_input(
     "Ask a question")
 
 output = st.empty()
 
-if query:
-    mm = chain.run(text=query)
 
-# print(result)
-# st.write(mm)
+if query:
+    results = qa(query)
+    print(results)
+    # mm = chain.run(text=query)
